0653fcb241eaf2a7f67cd29607ce59e54334672b
DexDexinWang <edgar_wdx@hotmail.com>
2018-02-06 09:18:58 -0800

    the final version

---
+++ app/src/main/java/com/laioffer/eventreporter/DataService.java
@@ -0,0 +1,18 @@
+package com.laioffer.eventreporter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Dex on 12/23/2017.
+ */
+
+public class DataService {
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<Event>();
+        for (int i = 0 ; i < 10 ; i++) {
+            eventData.add(new Event("Event", "1184 W valley Blvd, CA 90101","This is a huge event"));
+        }
+        return eventData;
+    }
+}
